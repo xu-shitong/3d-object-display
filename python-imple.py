f67cd46fe9130f07ad9c8a0d0f33044c93be0d67
shitong xu <tonyshengxue@outlook.com>
2021-08-18 21:46:31 +0100

    xst: 21:45 18/08: milestone of python donout, donout not clear, list has backfold

--- python-imple.py
+++ python-imple.py
@@ -1,25 +1,40 @@
 import numpy as np
 from math import pi, cos, sin, sqrt
+from time import sleep
+import curses
 
 def norm(v):
   return v / sum(v**2)**0.5
 
 # define hyperparameters
-canvas_height = 45
-canvas_width = 150
+canvas_height = 50
+canvas_width = 170
 R = 100
 r = 20
 h = 100
 d = 1000
-del_theta = 0.1
+del_theta = 0.05
 light_direction = np.array([0, 1, 0])
 light_direction = norm(light_direction)
 canvas_z = 300
-
-file = open("output.log", 'w')
+y_rotate_M = np.array([[cos(del_theta), 0, -sin(del_theta), 0], 
+                       [0,              1,               0, 0],
+                       [sin(del_theta), 0,  cos(del_theta), 0],
+                       [0,              0,               0, 1]])
+x_rotate_M = np.array([[1,              0,               0, 0], 
+                       [0, cos(del_theta), -sin(del_theta), 0],
+                       [0, sin(del_theta),  cos(del_theta), 0],
+                       [0,              0,               0, 1]])
+move_M = np.array([[1, 0, 0, 0],
+                   [0, 1, 0, h], 
+                   [0, 0, 1, d],
+                   [0, 0, 0, 1]])
+move_M_inverse = np.array([[1, 0, 0, 0],
+                           [0, 1, 0, -h], 
+                           [0, 0, 1, -d],
+                           [0, 0, 0, 1]])
 
 ILL_CHAR_MAP = ['.', ',', '-', ';', ':', ';', '=', '!', '*', '#', '$', '@']
-ILL_INT_MAP = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 class Point():
   def __init__(self, p) -> None:
@@ -44,76 +59,120 @@ class Point():
   
   # get char representing illuminus 
   def get_illumi_char(self):
-    illumi = self.get_illuminus() * len(ILL_CHAR_MAP)  # TODO: change back to char map
+    illumi = self.get_illuminus() * len(ILL_CHAR_MAP)
     # return str(int(illumi))
     return ILL_CHAR_MAP[int(illumi)]
 
-# generate donout pixel set
-pixels = []
-y_rotate_M_ = np.array([[cos(del_theta), 0, -sin(del_theta), 0], 
-                       [0, 1, 0, 0],
-                       [sin(del_theta), 0, cos(del_theta), 0],
-                       [0, 0, 0, 1]])
-move_M = np.array([[1, 0, 0, 0],
-                    [0, 1, 0, h], 
-                    [0, 0, 1, d],
-                    [0, 0, 0, 1]])
-# y_rotate_M = y_rotate_M_ / sum(y_rotate_M_**2)
-for alpha in np.arange(0, 2*pi, del_theta):
-  c_p = np.array([R + r * cos(alpha), r * sin(alpha), 0, 1])# pixel on the circle
-  for n in range(int(2*pi/del_theta)):
-    c_p = np.dot(y_rotate_M_, c_p) # pixel on the donout
-    d_p = np.dot(move_M, c_p)
-    pixels.append(Point(d_p))
+# file is used for debug propose, display donout to console instead
+# file = open("output.log", 'w')
 
-# mapping value to 2d
-point_matrix_2d = np.array([None] * canvas_width * canvas_height).reshape((canvas_height, canvas_width))
-for p in pixels:
-  x, y, z = p.x, p.y, p.z
-  ratio = canvas_z / z
-  x_trans = x * ratio
-  y_trans = y * ratio
+def get_image(pixels):
+  # mapping value to 2d
+  point_matrix_2d = np.array([None] * canvas_width * canvas_height).reshape((canvas_height, canvas_width))
+  for p in pixels:
+    x, y, z = p.x, p.y, p.z
+    ratio = canvas_z / z
+    x_trans = x * ratio
+    y_trans = y * ratio
+    
+    x_index, y_index = int(x_trans), int(y_trans)
+    # ignore pixel out off canvas
+    if (x_index + canvas_width // 2) >= canvas_width or y_index >= canvas_height:
+      continue
+
+    # file.write(str(x_trans) + ' ' + str(y_trans) + '\n')
+    # file.write(str(int(x_trans)) + ' ' + str(int(y_trans)) + '\n')
+
+    # showing pixel visible from viewer:
+    # # method 1: take the point with shortest distance from origin, where the viewer lies
+    # pre_point = point_matrix_2d[y_index, x_index]
+
+    # if (not pre_point) or (pre_point and pre_point.get_distance() > p.get_distance()):
+    #   point_matrix_2d[y_index, x_index + canvas_width // 2] = p
+
+    # # method 2: take the surface with norm of reverse direction of the view line
+    # if np.dot(np.array([x, y, z]), p.norm()) <= 0:
+    #   point_matrix_2d[y_index, x_index + canvas_width // 2] = p
+
+    # method 3: combine two approaches
+    pre_point = point_matrix_2d[y_index, x_index]
+
+    if np.dot(np.array([x, y, z]), p.norm()) <= 0:
+      # new point face the view direction
+      if (not pre_point) or (pre_point and pre_point.get_distance() > p.get_distance()):
+        # new point is closer to the viewer
+        point_matrix_2d[y_index, x_index + canvas_width // 2] = p
+
+
+  # get final ouput matrix by getting illuminus from each point
+  illuminus_matrix_2d = np.array([" "] * canvas_width * canvas_height).reshape((canvas_height, canvas_width))
+  for i in range(canvas_height):
+    for j in range(canvas_width):
+      if point_matrix_2d[i, j]:
+        illuminus_matrix_2d[canvas_height - i - 1, j] = point_matrix_2d[i, j].get_illumi_char()
   
-  x_index, y_index = int(x_trans), int(y_trans)
-  # ignore pixel out off canvas
-  if x_index >= canvas_width or y_index >= canvas_height:
-    continue
+  return illuminus_matrix_2d
 
-  # file.write(str(x_trans) + ' ' + str(y_trans) + '\n')
-  # file.write(str(int(x_trans)) + ' ' + str(int(y_trans)) + '\n')
+# getting (n, 4) np matrix for n points' position, 
+# return (n, 4) matrix of transformed position
+def rotate_donout(pixels_M):
+  # move donout back to origin
+  pixels_M = np.dot(pixels_M, move_M_inverse.T)
+  # rotate
+  xy_rotate_M = np.dot(x_rotate_M, y_rotate_M)
+  pixels_M = np.dot(pixels_M, xy_rotate_M.T)
 
-  # showing pixel visible from viewer:
-  # # method 1: take the point with shortest distance from origin, where the viewer lies
-  # pre_point = point_matrix_2d[y_index, x_index]
+  # move to the view position
+  return np.dot(pixels_M, move_M.T)
 
-  # if (not pre_point) or (pre_point and pre_point.get_distance() > p.get_distance()):
-  #   point_matrix_2d[y_index, x_index + canvas_width // 2] = p
+    
+# getting (n, 4) matrix, return list of n Pixels 
+def to_Pixels(pixels_M):
+  pixels = []
+  for v in pixels_M:
+    pixels.append(Point(v))
+  return pixels
 
-  # # method 2: take the surface with norm of reverse direction of the view line
-  # if np.dot(np.array([x, y, z]), p.norm()) <= 0:
-  #   point_matrix_2d[y_index, x_index + canvas_width // 2] = p
+def main(stdscr):
+  # initialize donout pixel set
+  pixels_M = []
 
-  # method 3: combine two approaches
-  pre_point = point_matrix_2d[y_index, x_index]
+  # y_rotate_M = y_rotate_M_ / sum(y_rotate_M_**2)
+  for alpha in np.arange(0, 2*pi, del_theta):
+    c_p = np.array([R + r * cos(alpha), r * sin(alpha), 0, 1])# pixel on the circle
+    for n in range(int(2*pi/del_theta)):
+      c_p = np.dot(y_rotate_M, c_p) # pixel on the donout
+      d_p = np.dot(move_M, c_p)
+      pixels_M.append(d_p)
 
-  if np.dot(np.array([x, y, z]), p.norm()) <= 0:
-    # new point face the view direction
-    if (not pre_point) or (pre_point and pre_point.get_distance() > p.get_distance()):
-      # new point is closer to the viewer
-      point_matrix_2d[y_index, x_index + canvas_width // 2] = p
 
+  # for i in range(5):
+  #   pixels_M = rotate_donout(pixels_M)
 
-# get final ouput matrix by getting illuminus from each point
-illuminus_matrix_2d = np.array([" "] * canvas_width * canvas_height).reshape((canvas_height, canvas_width))
-for i in range(canvas_height):
-  for j in range(canvas_width):
-    if point_matrix_2d[i, j]:
-      illuminus_matrix_2d[canvas_height - i - 1, j] = point_matrix_2d[i, j].get_illumi_char()
-      
-# generate function define reflection from light ray
-for j in range(27): # emperically after line 27 no pixels are shown
-  list = illuminus_matrix_2d[j]
-  for i in list:
-    file.write(i)
-  file.write('\n')
-file.close()
+  # pixels = to_Pixels(pixels_M)
+  # illuminus_matrix_2d = get_image(pixels)
+  # # output to file
+  # for j in range(len(illuminus_matrix_2d)): 
+  #   list = illuminus_matrix_2d[j]
+  #   for i in list:
+  #     file.write(i)
+  #   file.write('\n')
+  # file.close()
+
+  # printing the rotating donout till program killed
+  # while True:
+  for i in range(100):
+
+    pixels = to_Pixels(pixels_M)
+    illuminus_matrix_2d = get_image(pixels)
+    
+    for j in range(len(illuminus_matrix_2d)): 
+      line = illuminus_matrix_2d[j]
+      # print(''.join(line))
+      stdscr.addstr(j, 0, ''.join(line))
+    stdscr.refresh()
+    sleep(0.05)
+    pixels_M = rotate_donout(pixels_M)
+curses.wrapper(main)
+
+# main(None)
